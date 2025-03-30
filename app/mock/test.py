e4c434f12a33ce9b1cacc7a17fa0efb484485eec
phuc297 <dhfuc7@gmail.com>
2025-03-30 22:46:54 +0700

    Removed ignored files from Git tracking

--- app/mock/test.py
+++
@@ -1,9 +0,0 @@
-from faker import Faker
-import random
-
-# Chọn một số ngẫu nhiên trong khoảng từ 1 đến 10
-num = random.choice(range(10, 50))
-print(f"Số được chọn: {num}")
-# Khởi tạo Faker
-fake = Faker()
-print(fake.sentence(num))
--- app/posts/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from . import views
-
-urlpatterns = [
-    path('<int:post_id>', views.post_view, name='post_view'),
-]
