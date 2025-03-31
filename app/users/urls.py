190c1fc602d83b7647d4bfda8d57bef14a36ff0d
phuc297 <dhfuc7@gmail.com>
2025-04-01 00:32:40 +0700

    fix comment posting

--- app/users/urls.py
+++ app/users/urls.py
@@ -1,10 +1,10 @@
-from django.urls import path, include
-from django.views.generic import TemplateView
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('logout/', views.logout_view, name='logout'),
-    path('signup/', views.signup_view, name='signup'),
-    path('<int:profile_id>', views.profile_view, name='profile'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('signup/', views.SignupView.as_view(), name='signup'),
+    path('<int:pk>', views.ProfileView.as_view(), name='profile'),
 ]
